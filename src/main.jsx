1a9d4ee270a9acefe0a7c8b2ed38062b0fa2e094
dev-ahmed-hatem <ahmedhatemezzathelal@gmail.com>
2024-08-19 23:06:16 +0300

    strict and flowbite

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,19 @@
-// import { StrictMode } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Flowbite } from "flowbite-react";
+import theme from "./assets/FlowbiteTheme";
 import "./css/index.css";
 
 createRoot(document.getElementById("root")).render(
-    <div className="font-bold text-center text-3xl">Main Content</div>
+    <StrictMode>
+        <Flowbite theme={{ theme: theme }}>
+            <div className="font-bold text-center text-3xl">Main Content</div>
+        </Flowbite>
+    </StrictMode>
 );
+
+{
+    /* <RouterProvider
+    router={createBrowserRouter(browserRoutes)}
+></RouterProvider> */
+}
