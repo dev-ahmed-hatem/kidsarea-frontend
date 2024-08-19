5ed5672e5c40d9af51a1aede0c1e7de3b67ac2e0
dev-ahmed-hatem <ahmedhatemezzathelal@gmail.com>
2024-08-20 00:03:32 +0300

    add .sx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Flowbite } from "flowbite-react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import { browserRoutes } from "./constants/Routing";
+import { browserRoutes } from "./constants/Routing.jsx";
 import theme from "./assets/FlowbiteTheme";
 import "./css/index.css";
 
