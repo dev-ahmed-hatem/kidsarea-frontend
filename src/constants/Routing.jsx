a91cf1d4e74f9b28928318f1c27335f6727737a3
dev-ahmed-hatem <ahmedhatemezzathelal@gmail.com>
2024-08-19 23:49:05 +0300

    hash main and index

--- src/constants/Routing.jsx
+++ src/constants/Routing.jsx
@@ -1,7 +1,7 @@
 import Section from "../components/sections/Section";
 import Subsection from "../components/sections/Subsection";
 import NotFound from "../NotFound";
-import Main from "../components/main";
+// import Main from "../components/main";
 // import { routes } from "./Index";
 import Login from "../components/login/Login";
 
